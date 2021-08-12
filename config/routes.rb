15150662e39a9d95df412c1532ee4fbea2db8bdd
jreyes8631 <jreyes8631@gmail.com>
2021-08-11 22:00:02 -0400

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
   get "about", to: "about#index"
   root to: "main#index"
 
