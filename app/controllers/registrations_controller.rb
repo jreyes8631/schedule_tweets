419a3603f02cd66e14ce0f7538fd94d3319d8797
jreyes8631 <jreyes8631@gmail.com>
2021-08-19 17:20:46 -0400

    add create action and privite user params method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,20 @@ class RegistrationsController < ApplicationController
        @user = User.new
     end
 
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: 'User was successfully created'
+        else 
+            flash[:alert] = "something went wrong"
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+       params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
 end
