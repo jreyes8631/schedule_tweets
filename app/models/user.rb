cb8b62d9874a0a1155f1e527ac6cfd2d4032a8b6
jreyes8631 <jreyes8631@gmail.com>
2021-08-29 23:05:18 -0400

    validating emial with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence: true
+    validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, messages: "Must be a valid email"}
 end
