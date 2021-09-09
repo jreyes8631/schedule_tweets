96171b455bcab30e21561df5a8c2c8c589732d15
jreyes8631 <jreyes8631@gmail.com>
2021-09-08 21:10:56 -0400

    add route to password update

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@
 
 Rails.application.routes.draw do
   get "about", to: "about#index"
-  root to: "main#index"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "signed_up", to: "registrations#new"
   post "signed_up", to: "registrations#create"
 
   delete "logout", to: "sessions#destroy"
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
+
+  root to: "main#index"
 
 end
